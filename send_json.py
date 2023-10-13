8ced1eaf50c4c600351949031a4380dc4e2a71d7
ujjwalpawar <ujjwalpawar72@gmail.com>
2023-10-13 13:05:02 +0100

    added send json test script

---
+++ send_json.py
@@ -0,0 +1,41 @@
+import socket
+import json
+import time
+
+# Function to read JSON objects from a file and send them to the server
+def send_json_objects(file_path, server_address, server_port):
+    with open('raw', 'r') as file:
+        json_objects = file.read().splitlines()
+
+    # Create a TCP/IP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the server's address and port
+    client_socket.connect((server_address, server_port))
+
+    try:
+        for json_object in json_objects:
+            # Parse the JSON object
+            parsed_json = json.loads(json_object)
+
+            # Send the JSON object to the server
+            client_socket.sendall(json.dumps(parsed_json).encode())
+            print(f'Sent: {parsed_json}')
+
+            # Wait for 1 millisecond before sending the next JSON object
+            time.sleep(0.001)
+
+    finally:
+        # Clean up the connection
+        client_socket.close()
+
+# Server address and port
+server_address = '10.244.1.4'
+server_port = 12345
+
+# File containing concatenation of multiple JSON objects
+file_path = 'json_objects.txt'
+
+# Call the function to send JSON objects to the server
+send_json_objects(file_path, server_address, server_port)
+
